364c96ed37d341ba0c1e2c393725942b7b40000a
gma992 <gabrielmaldonado.art@gmail.com>
2016-03-04 18:22:19 +0900

    fixed for multiple pages

--- WP-test-check-pages.js
+++ WP-test-check-pages.js
@@ -3,12 +3,23 @@ var baseURL = 'http://dev.gabrielmaldonado.me'
 // Access to login and try a wrong username and password
 casper.test.begin('TEST: Checking if the pages are in good standing', function(){
 
+	// array with the pages in the website
+	websitePages = ['/web-development', '/seo', '/performance-speed-optimization', '/testimonials-and-feedback', '/contact'];
+
+	//check homepage or base url
 	casper.start(baseURL, function(){
-		// check if title empty
 		this.test.assert(this.getTitle()!='', 'Title exists!');
-		//console.log('Page: ' + this.echo(this.getTitle()));
 		console.log('Page: ' + this.echo(this.getTitle()));
-		//check if baseURL +  /web-development/ exists
+	});
+
+	//check the rest
+	casper.then(function(){
+		for (var i = websitePages.length - 1; i >= 0; i--) {
+			casper.thenOpen(baseURL + websitePages[i], function(){
+				this.test.assert(this.getTitle()!='', 'Title exists!');
+				console.log('Page: ' + this.echo(this.getTitle()));		
+			})
+		}
 	});
 
 });
